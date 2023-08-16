af200b95ae2321830a4147ed944ecedfc6ab0646
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-08-16 13:29:19 +0800

    init push

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>LunarStore Documentation</span>,
+  project: {
+    link: 'https://github.com/lunastore',
+  },
+  docsRepositoryBase: 'https://github.com/lunastore',
+  footer: {
+    text: 'LunarStore Documentation',
+  },
+}
+
+export default config
